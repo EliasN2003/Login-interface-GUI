2d6a30518d203bf68c2d59f2006f949a786c1158
eliasn2003 <110858094+EliasN2003@users.noreply.github.com>
2022-09-10 02:31:18 +0300

    Created signup interface
    Created signup interface and made login and signup buttons functional.

--- Login interface GUI.py
+++ Login interface GUI.py
@@ -5,26 +5,92 @@ from tkinter import *
 
 root = Tk()
 root.title("Login interface")
-root.geometry("800x460")
+root.geometry("900x550")
+root.configure(bg="#000000")
+root.resizable(False, False)
 
-# Title
-title = Label(root, text="COMPANY NAME", font=("Times New Roman", 30, "bold"))
-title.grid(row=0, column=0, columnspan=2, pady=5)
-root.grid_columnconfigure(0, weight=1)
-root.grid_columnconfigure(1, weight=1)
-divider = Label(root)
-divider.grid(row=1, column=0, columnspan=2, pady=7)
+
+# Signup interface
+def signup():
+    # Clears the main window to print the results page
+    for widget in root.winfo_children():
+        widget.grid_forget()
+
+    # Title
+    title = Label(root, text="BANK", font=("Palatino Linotype", 30, "bold"), bg="#000000", fg="#FFFFFF")
+    title.grid(row=1, column=0, columnspan=2)
+    root.grid_columnconfigure(0, weight=1)
+    root.grid_columnconfigure(1, weight=1)
+
+    login_label = Label(root, text="Sign up for free", font=("Palatino Linotype", 18, "bold"), bg="#000000",
+                        fg="#FFFFFF")
+    login_label.grid(row=3, column=0, columnspan=2, pady=20)
+    username_label = Label(root, text="Username: ", font=("Palatino Linotype", 17, "bold"), bg="#000000", fg="#FFFFFF")
+    username_label.grid(row=4, column=0, sticky=E)
+    password_label = Label(root, text="Password: ", font=("Palatino Linotype", 17, "bold"), bg="#000000", fg="#FFFFFF")
+    password_label.grid(row=5, column=0, sticky=E, pady=10)
+    password_repeat_label = Label(root, text="Repeat password: ", font=("Palatino Linotype", 17, "bold"), bg="#000000",
+                                  fg="#FFFFFF")
+    password_repeat_label.grid(row=6, column=0, sticky=E)
+    username_entrybar = Entry(root, borderwidth=3, font=("Palatino Linotype", 15), bg="#000000", fg="#FFFFFF")
+    username_entrybar.grid(row=4, column=1, sticky=W)
+    password_entrybar = Entry(root, borderwidth=3, font=("Palatino Linotype", 15), bg="#000000", fg="#FFFFFF")
+    password_entrybar.grid(row=5, column=1, sticky=W)
+    password_repeat_entrybar = Entry(root, borderwidth=3, font=("Palatino Linotype", 15), bg="#000000", fg="#FFFFFF")
+    password_repeat_entrybar.grid(row=6, column=1, columnspan=2, sticky=W)
+    signup_button = Button(root, text="Sign up", font=("Palatino Linotype", 16, "bold"), borderwidth=3, bg="#000000",
+                           fg="#FFFFFF")
+    signup_button.grid(row=7, column=0, columnspan=2, ipadx=8, pady=20)
+    divider = Label(root, bg="#000000")
+    divider.grid(row=8, column=0, columnspan=2, pady=30)
+    login_label = Label(root, text="Already have an account?", font=("Palatino Linotype", 16, "bold"), borderwidth=3,
+                         bg="#000000", fg="#FFFFFF")
+    login_label.grid(row=9, column=0, columnspan=2)
+    login_button = Button(root, text="Login", font=("Palatino Linotype", 16, "bold"), borderwidth=3, bg="#000000",
+                          fg="#FFFFFF", command=login)
+    login_button.grid(row=10, column=0, columnspan=2)
+
 
 # Login interface
-username_label = Label(root, text="Username: ", font=("Arial", 17, "bold"))
-username_label.grid(row=2, column=0, sticky=E)
-password_label = Label(root, text="Password: ", font=("Arial", 17, "bold"))
-password_label.grid(row=3, column=0, sticky=E, pady=10)
-username_entrybar = Entry(root, borderwidth=3, font=("Arial", 15))
-username_entrybar.grid(row=2, column=1, sticky=W)
-password_entrybar = Entry(root, borderwidth=3, font=("Arial", 15))
-password_entrybar.grid(row=3, column=1, sticky=W)
-login_button = Button(root, text="Login", font=("Times New Roman", 16, "bold"), borderwidth=3)
-login_button.grid(row=4, column=0, columnspan=2, ipadx=8, pady=15)
+def login():
+    # Clears the main window
+    for widget in root.winfo_children():
+        widget.grid_forget()
+
+    # Title
+    title = Label(root, text="BANK", font=("Palatino Linotype", 30, "bold"), bg="#000000", fg="#FFFFFF")
+    title.grid(row=1, column=0, columnspan=2)
+    root.grid_columnconfigure(0, weight=1)
+    root.grid_columnconfigure(1, weight=1)
+
+    login_label = Label(root, text="Log in to your account", font=("Palatino Linotype", 18, "bold"), bg="#000000",
+                        fg="#FFFFFF")
+    login_label.grid(row=3, column=0, columnspan=2, pady=20)
+    username_label = Label(root, text="Username: ", font=("Palatino Linotype", 17, "bold"), bg="#000000", fg="#FFFFFF")
+    username_label.grid(row=4, column=0, sticky=E)
+    password_label = Label(root, text="Password: ", font=("Palatino Linotype", 17, "bold"), bg="#000000", fg="#FFFFFF")
+    password_label.grid(row=5, column=0, sticky=E, pady=10)
+    username_entrybar = Entry(root, borderwidth=3, font=("Palatino Linotype", 15), bg="#000000", fg="#FFFFFF")
+    username_entrybar.grid(row=4, column=1, sticky=W)
+    password_entrybar = Entry(root, borderwidth=3, font=("Palatino Linotype", 15), bg="#000000", fg="#FFFFFF")
+    password_entrybar.grid(row=5, column=1, sticky=W)
+    login_button = Button(root, text="Login", font=("Palatino Linotype", 16, "bold"), borderwidth=3, bg="#000000",
+                          fg="#FFFFFF")
+    login_button.grid(row=6, column=0, columnspan=2, ipadx=8, pady=15)
+    divider = Label(root, bg="#000000")
+    divider.grid(row=7, column=0, columnspan=2, pady=53)
+    signup_label = Label(root, text="Dont have an account?", font=("Palatino Linotype", 16, "bold"), borderwidth=3,
+                         bg="#000000", fg="#FFFFFF")
+    signup_label.grid(row=8, column=0, columnspan=2)
+    signup_button = Button(root, text="Sign up", font=("Palatino Linotype", 16, "bold"), borderwidth=3, bg="#000000",
+                           fg="#FFFFFF", command=signup)
+    signup_button.grid(row=9, column=0, columnspan=2)
+
+
+login()
+
 
 root.mainloop()
+
+# Comments:
+# Add dark mode button
