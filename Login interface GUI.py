e183434523b8d55dda7277cfa1dd2d83b992beff
eliasn2003 <110858094+EliasN2003@users.noreply.github.com>
2022-09-20 08:07:29 +0300

    Save
    Saving to continue from my laptop

--- Login interface GUI.py
+++ Login interface GUI.py
@@ -2,6 +2,8 @@
 # LOGIN INTERFACE
 
 from tkinter import *
+from tkinter import messagebox
+
 
 root = Tk()
 root.title("Login interface")
@@ -17,17 +19,23 @@ theme = 1
 back = "#FFFFFF"
 fore = "#000000"
 lamp = lamp_on
-cur_color = fore
+
+# Variables used for creating users and logging in
+username = StringVar()
+password = StringVar()
+password_confirm = StringVar()
 
 
+# Dark/light mode for the signup page
 def signup_themechange():
     light_dark_mode()
     signup()
 
 
+# Dark/light mode for the login page
 def login_themechange():
     light_dark_mode()
-    login()
+    login_interface()
 
 
 # Sets light and dark mode
@@ -43,8 +51,6 @@ def light_dark_mode():
         root.configure(bg="#000000")
         global lamp
         lamp = lamp_off
-        global cur_color
-        cur_color = "#000000"
 
 
     # Sets light mode
@@ -54,7 +60,14 @@ def light_dark_mode():
         theme = theme + 1
         root.configure(bg="#FFFFFF")
         lamp = lamp_on
-        cur_color = "#000000"
+
+
+def login():
+    return
+
+
+def create_user():
+    return
 
 
 # Signup interface
@@ -81,32 +94,34 @@ def signup():
     password_repeat_label = Label(root, text="Repeat password: ", font=("Palatino Linotype", 17, "bold"),
                                   bg=back, fg=fore)
     password_repeat_label.grid(row=4, column=0, sticky=E)
-    username_entrybar = Entry(root, borderwidth=3, font=("Palatino Linotype", 15), bg=back, fg=fore)
+    username_entrybar = Entry(root, textvariable=username, borderwidth=3, font=("Palatino Linotype", 15), bg=back,
+                              fg=fore)
     username_entrybar.grid(row=2, column=1, sticky=W)
-    password_entrybar = Entry(root, borderwidth=3, font=("Palatino Linotype", 15), bg=back, fg=fore)
+    password_entrybar = Entry(root, textvariable=password, borderwidth=3, font=("Palatino Linotype", 15), bg=back,
+                              fg=fore)
     password_entrybar.grid(row=3, column=1, sticky=W)
-    password_repeat_entrybar = Entry(root, borderwidth=3, font=("Palatino Linotype", 15), bg=back,
-                                     fg=fore)
+    password_repeat_entrybar = Entry(root, textvariable=password_confirm, borderwidth=3, font=("Palatino Linotype", 15),
+                                     bg=back, fg=fore)
     password_repeat_entrybar.grid(row=4, column=1, columnspan=2, sticky=W)
-    signup_button = Button(root, text="Sign up", font=("Palatino Linotype", 16, "bold"), borderwidth=3,
-                           bg=back, fg=fore)
+    signup_button = Button(root, text="Sign up", font=("Palatino Linotype", 16, "bold"), cursor='hand2', borderwidth=3,
+                           bg=back, fg=fore, command=create_user)
     signup_button.grid(row=5, column=0, columnspan=2, ipadx=8, pady=20)
-    divider = Label(root, bg=back)
-    divider.grid(row=6, column=0, columnspan=2, pady=30)
+    user_created_holder = Label(root, text="", font=("Palatino Linotype", 16, "bold"), bg=back)
+    user_created_holder.grid(row=6, column=0, columnspan=2, ipady=10, ipadx=15)
     login_label = Label(root, text="Already have an account?", font=("Palatino Linotype", 16, "bold"),
                         borderwidth=3, bg=back, fg=fore)
-    login_label.grid(row=7, column=0, columnspan=2)
-    login_button = Button(root, text="Login", font=("Palatino Linotype", 16, "bold"), borderwidth=3, command=login,
-                          bg=back, fg=fore)
+    login_label.grid(row=7, column=0, columnspan=2, pady=(27, 0), sticky=N)
+    login_button = Button(root, text="Login", font=("Palatino Linotype", 16, "bold"), cursor='hand2', borderwidth=3,
+                          command=login_interface, bg=back, fg=fore)
     login_button.grid(row=8, column=0, columnspan=2)
 
     # Dark mode button
-    dark_button = Button(root, image=lamp, command=signup_themechange, bg=back)
+    dark_button = Button(root, image=lamp, command=signup_themechange, cursor='hand2', bg=back)
     dark_button.grid(row=0, column=1, sticky=E, padx=5)
 
 
 # Login interface
-def login():
+def login_interface():
     # Clears the main window
     for widget in root.winfo_children():
         widget.grid_forget()
@@ -127,24 +142,25 @@ def login():
     username_entrybar.grid(row=2, column=1, sticky=W)
     password_entrybar = Entry(root, borderwidth=3, font=("Palatino Linotype", 15), bg=back, fg=fore)
     password_entrybar.grid(row=3, column=1, sticky=W)
-    login_button = Button(root, text="Login", font=("Palatino Linotype", 16, "bold"), borderwidth=3, bg=back, fg=fore)
+    login_button = Button(root, text="Login", font=("Palatino Linotype", 16, "bold"), cursor='hand2', borderwidth=3,
+                          bg=back, fg=fore, command=login)
     login_button.grid(row=4, column=0, columnspan=2, ipadx=8, pady=15)
     divider = Label(root, bg=back, fg=fore)
     divider.grid(row=5, column=0, columnspan=2, pady=53)
-    signup_label = Label(root, text="Dont have an account?", font=("Palatino Linotype", 16, "bold"),
+    signup_label = Label(root, text="Don't have an account?", font=("Palatino Linotype", 16, "bold"),
                          borderwidth=3, bg=back, fg=fore)
     signup_label.grid(row=6, column=0, columnspan=2)
-    signup_button = Button(root, text="Sign up", font=("Palatino Linotype", 16, "bold"), borderwidth=3,
+    signup_button = Button(root, text="Sign up", font=("Palatino Linotype", 16, "bold"), cursor='hand2', borderwidth=3,
                            command=signup, bg=back, fg=fore)
     signup_button.grid(row=7, column=0, columnspan=2)
 
     # Dark mode button
-    dark_button = Button(root, image=lamp, command=login_themechange, bg=back)
+    dark_button = Button(root, image=lamp, command=login_themechange, cursor='hand2', bg=back)
     dark_button.grid(row=0, column=1, sticky=E, padx=5)
 
 
 # Runs the program
-login()
+login_interface()
 
 
 root.mainloop()
